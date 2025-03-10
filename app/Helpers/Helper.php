f0b2ad59c1bc2fce2270ca5275834508faf34cd6
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-03-10 20:18:28 +0700

    helper update encode decode

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,16 +1,31 @@
 <?php
 
 namespace App\Helpers;
+use Illuminate\Support\Facades\Crypt;
 
 class Helper
 {
+    // public static function encodeId($id)
+    // {
+    //     return base64_encode($id);
+    // }
+
+    // public static function decodeId($encodedId)
+    // {
+    //     return base64_decode($encodedId);
+    // }
+
+    private static $salt = 'P3Z0Q'; // Ganti dengan salt yang aman
     public static function encodeId($id)
     {
-        return base64_encode($id);
+        $idWithSalt = $id . self::$salt; // Tambahkan salt
+        return base64_encode($idWithSalt);
     }
 
     public static function decodeId($encodedId)
     {
-        return base64_decode($encodedId);
+        $decoded = base64_decode($encodedId);
+        return str_replace(self::$salt, '', $decoded); // Hapus salt
     }
+
 }

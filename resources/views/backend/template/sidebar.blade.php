9cc9eba0f4278137646342fb4926a2f5d5fe0469
Edwin <prayogo.edwin@gmail.com>
2025-12-01 20:30:07 +0700

    update dashboardd

--- resources/views/backend/template/sidebar.blade.php
+++ resources/views/backend/template/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="index.html" class="app-brand-link">
+        <a href="{{ route('dashboard') }}" class="app-brand-link">
             <span class="app-brand-logo demo">
                 <img src="{{ asset('LOGO_SENGKUYUNG/LOGO_SENGKUYUNG.png') }}" width="200px">
             </span>
